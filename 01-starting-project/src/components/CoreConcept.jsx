bdbd468a72b762a2e2d2100bb20853f3961dcea6
Natalina-Yarosh <natali.yarosh.96@mail.ru>
2025-01-10 19:12:20 +0300

    update branch

--- 01-starting-project/src/components/CoreConcept.jsx
+++ 01-starting-project/src/components/CoreConcept.jsx
@@ -1,11 +1,9 @@
-import './CoreConcept.css'
-
-export default function CoreConcept({title, image, description}) {
-    return (
-        <li>
-            <img src={image} alt={title}/>
-            <h3>{title}</h3>
-            <p>{description}</p>
-        </li>
-    )
+export default function CoreConcept({ image, title, description }) {
+  return (
+    <li>
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
+  );
 }
