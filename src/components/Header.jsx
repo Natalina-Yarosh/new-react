59faa5dd252ccd457a549f93b2debda2dba9cb57
Natalina-Yarosh <natali.yarosh.96@mail.ru>
2025-02-06 20:29:29 +0300

    Getting Started with a User Input Component

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+import logo from '../assets/investment-calculator-logo.png'
+
+export default function Header() {
+    return <header id='header'>
+        <img src={logo} alt="Investment calculator"/>
+        <h1>Investment calculator</h1>
+    </header>
+}
