b032d3b2a25e25d79d9cb7b753016c05eed76ecf
Natalina-Yarosh <natali.yarosh.96@mail.ru>
2025-02-07 09:45:17 +0300

    Handling Events & Using Two-Way-Binding

--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -21,21 +21,21 @@ export default function UserInput() {
         <div className='input-group'>
             <p>
                 <label htmlFor="">Initial Investment</label>
-                <input type="number" required onChange={() => handleChange('initialInvestment')} />
+                <input type="number" required value={userInput.initialInvestment} onChange={(event) => handleChange('initialInvestment', event.target.value)} />
             </p>
             <p>
                 <label htmlFor="">Annual Investment</label>
-                <input type="number" required/>
+                <input type="number" required value={userInput.annualInvestment} onChange={(event) => handleChange('annualInvestment', event.target.value)}/>
             </p>
         </div>
         <div className='input-group'>
             <p>
                 <label htmlFor="">Expected Return</label>
-                <input type="number" required/>
+                <input type="number" required value={userInput.expectedReturn} onChange={(event) => handleChange('expectedReturn', event.target.value)}/>
             </p>
             <p>
                 <label htmlFor="">Duration</label>
-                <input type="number" required/>
+                <input type="number" required value={userInput.duration} onChange={(event) => handleChange('duration', event.target.value)}/>
             </p>
         </div>
     </section>
