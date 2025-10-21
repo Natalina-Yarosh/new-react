ccffae99cc73ef6e0c6b07f04fcc6702d86f09c0
Natalina-Yarosh <natali.yarosh.sergeevna@gmail.com>
2025-10-22 01:11:20 +0300

    Using Refs for More Than "DOM Element Connections"

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,13 +1,14 @@
 import { useRef, useState } from "react";
 
 export default function TimerChallenge({title, targetTime}) {
-
+ const timer = useRef()
   const [timerStarted, setTimerStarted] = useState(false);
   const [timerExpired, setTimerExpired] = useState(false);
 
+ 
   function handleStart(){
   
-    setTimeout(() => {
+     timer.current = setTimeout(() => {
       setTimerExpired(true)
     }, targetTime * 1000)
 
@@ -15,7 +16,7 @@ export default function TimerChallenge({title, targetTime}) {
   }
 
   function handleStop(){
-    
+    clearTimeout(timer.current)
   }
 
   return (
@@ -25,7 +26,7 @@ export default function TimerChallenge({title, targetTime}) {
      <p className="challenge-time">
        {targetTime} second{targetTime > 1 ? 's' : ''} 
       </p>
-      <button onClick={handleStart}>
+      <button onClick={timerStarted ? handleStop :  handleStart}>
         {timerStarted ? 'Stop' : 'Start'} challenge
       </button>
       <p className={timerStarted ? 'active' : undefined}>
