b446971ced186041c372151fd9320ced6fd7255c
Natalina-Yarosh <natali.yarosh.sergeevna@gmail.com>
2025-10-30 00:49:02 +0300

    Migrating the Entire Demo Project to use the Context API

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,8 +1,8 @@
 import { useContext } from "react"; //import { use} in react 19 >
 import { CartContext } from "../store/shopping-cart-context";
 
-export default function Cart({ onUpdateItemQuantity }) {
-  const {items} = useContext(CartContext); //use(CartContext) in react 19 >
+export default function Cart() {
+  const {items, updateItemQuantity} = useContext(CartContext); //use(CartContext) in react 19 >
 
   const totalPrice = items.reduce(
     (acc, item) => acc + item.price * item.quantity,
@@ -25,11 +25,11 @@ export default function Cart({ onUpdateItemQuantity }) {
                   <span> ({formattedPrice})</span>
                 </div>
                 <div className="cart-item-actions">
-                  <button onClick={() => onUpdateItemQuantity(item.id, -1)}>
+                  <button onClick={() => updateItemQuantity(item.id, -1)}>
                     -
                   </button>
                   <span>{item.quantity}</span>
-                  <button onClick={() => onUpdateItemQuantity(item.id, 1)}>
+                  <button onClick={() => updateItemQuantity(item.id, 1)}>
                     +
                   </button>
                 </div>
