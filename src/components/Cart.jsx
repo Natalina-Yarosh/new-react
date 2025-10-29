473a237d8d885976d3bf264e63afdd664e8c544b
Natalina-Yarosh <natali.yarosh.sergeevna@gmail.com>
2025-10-30 00:22:18 +0300

    Consuming the Context

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,4 +1,9 @@
-export default function Cart({ items, onUpdateItemQuantity }) {
+import { useContext } from "react"; //import { use} in react 19 >
+import { CartContext } from "../store/shopping-cart-context";
+
+export default function Cart({ onUpdateItemQuantity }) {
+  const {items} = useContext(CartContext); //use(CartContext) in react 19 >
+
   const totalPrice = items.reduce(
     (acc, item) => acc + item.price * item.quantity,
     0
